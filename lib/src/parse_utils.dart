8a528f68bc3bd2596a3b04c32ffc31920bac08e6
Harry Stern <hcs@meow.sh>
2018-02-28 14:35:56 -0500

    Run dartfmt on parser_utils.dart

--- lib/src/parse_utils.dart
+++ lib/src/parse_utils.dart
@@ -1,36 +1,36 @@
 import 'package:xml/xml.dart' as xml;
 
 String attr_to_string(String attribute, xml.XmlElement node) {
-	var attr = node.getAttribute(attribute);
-	if (attr == null) {
-		attr = "";
-	}
+  var attr = node.getAttribute(attribute);
+  if (attr == null) {
+    attr = "";
+  }
 
-	return attr;
+  return attr;
 }
 
 int attr_to_int(String attribute, xml.XmlElement node) {
-	var attr_ = node.getAttribute(attribute);
-	var attr = null;
-	if (attr_ != null) {
-		attr = int.parse(attr_);
-	}
+  var attr_ = node.getAttribute(attribute);
+  var attr = null;
+  if (attr_ != null) {
+    attr = int.parse(attr_);
+  }
 
-	return attr;
+  return attr;
 }
 
 num attr_to_num(String attribute, xml.XmlElement node) {
-	var attr_ = node.getAttribute(attribute);
-	var attr = null;
-	if (attr_ != null) {
-		attr = num.parse(attr_);
-	}
+  var attr_ = node.getAttribute(attribute);
+  var attr = null;
+  if (attr_ != null) {
+    attr = num.parse(attr_);
+  }
 
-	return attr;
+  return attr;
 }
 
 bool attr_to_bool(String attribute, xml.XmlElement node) {
-	var attr_ = attr_to_string(attribute, node);
+  var attr_ = attr_to_string(attribute, node);
 
-	return (attr_ == "true" || attr_ == "1");
+  return (attr_ == "true" || attr_ == "1");
 }
